d1961a56ec677a57483a115dd4214e7696b640fb
Dan Sakamoto <contact@dansakamoto.com>
2024-02-02 15:33:39 -0500

    remove unused ui elements

--- src/sketch.js
+++ src/sketch.js
@@ -3,7 +3,7 @@ import anteater01 from "./assets/images/anteater01.png";
 import anteater02 from "./assets/images/anteater02.png";
 import scremSound from "./assets/sounds/screm.mp3";
 
-let anteater, store, input, button, history;
+let store;
 
 window.preload = () => {
   store = loadImage(grocerystore);
@@ -12,20 +12,6 @@ window.preload = () => {
 window.setup = () => {
   createCanvas(windowWidth / 2, windowHeight);
   window.anteater = new Anteater(50, 50);
-
-  textSize(windowWidth / 45);
-  textWrap(WORD);
-
-  input = createInput();
-  input.position(800, 50);
-
-  button = createButton("Ask");
-  button.position(input.x + input.width + 5, input.y);
-  button.mousePressed(ask);
-
-  history = "";
-
-  input.value("");
 };
 
 window.windowResized = () => {
@@ -36,17 +22,12 @@ window.draw = () => {
   background(220);
   image(store, 0, 0);
   window.anteater.draw();
-
-  text(history, input.x, input.y + 50, 400);
 };
 
 window.ask = (msg) => {
   var item = document.createElement("li");
   item.textContent = msg;
   return item;
-  // const question = input.value();
-  // history = question + "\n" + history;
-  // input.value('');
 };
 
 window.reply = () => {
